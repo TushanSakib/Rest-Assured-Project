3c8a50bccc212dcd0a0caecf2612fd24da90d471
TushanSakib <abdulazizsakib853@gmail.com>
2026-07-13 20:07:19 +0600

    Created routes, configreader,qa properties

---
+++ src/main/java/endpoints/Routes.java
@@ -0,0 +1,10 @@
+package endpoints;
+
+public class Routes {
+    public static final String GET_USERS ="/api/users?page=2";
+    public static final String GET_SINGLE_USER = "/api/users/{id}";
+    public static final String CREATE_USER ="/api/users";
+    public static final String UPDATE_USER ="/api/users/{id}";
+    public static final String DELETE_USER ="/api/users/{id}";
+    public static final String LOGIN ="/api/login";
+}
