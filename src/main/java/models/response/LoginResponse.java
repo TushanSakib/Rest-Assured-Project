c0b2bae59821f4d7eb89c411982ee5414c9034c2
TushanSakib <abdulazizsakib853@gmail.com>
2026-07-19 16:20:06 +0600

    Login req and res

---
+++ src/main/java/models/response/LoginResponse.java
@@ -0,0 +1,18 @@
+package models.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LoginResponse {
+
+    private String token;
+
+    public String getToken(){
+        return token;
+    }
+
+    public void setToken(String token){
+        this.token=token;
+    }
+
+}
